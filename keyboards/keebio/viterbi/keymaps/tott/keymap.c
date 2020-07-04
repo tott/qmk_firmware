8f35817f7e9f23a95b2e24bb87221663d0536a69
Thorsten Ott <thorsten.ott@10up.com>
2020-07-04 17:43:19 +0200

    Reformat and shift on right

--- keyboards/keebio/viterbi/keymaps/tott/keymap.c
+++ keyboards/keebio/viterbi/keymaps/tott/keymap.c
@@ -38,32 +38,32 @@ const uint32_t PROGMEM unicode_map[] = {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_QWERTY] = LAYOUT_ortho_5x14(
-            KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,     KC_5,     KC_6,          KC_7,    KC_8,     KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC,
-            KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,     KC_T,     KC_LBRC,       KC_RBRC, KC_Y,     KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,
-            KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,     KC_G,     KC_HOME,       KC_DEL,  KC_H,     KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-            KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,     KC_B,     KC_END,        KC_INS,  KC_N,     KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT,
-            KC_LCTL, KC_LGUI, KC_LALT, MO(1),   TO(1),    MO(2),    KC_SPC,        KC_SPC,  MO(1),    TO(3),   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT),
+KC_GRV,   KC_1,         KC_2,     KC_3,     KC_4,      KC_5,      KC_6,         KC_7,     KC_8,      KC_9,         KC_0,     KC_MINS,      KC_EQL,   KC_BSPC,
+KC_TAB,   KC_Q,         KC_W,     KC_E,     KC_R,      KC_T,      KC_LBRC,      KC_RBRC,  KC_Y,      KC_U,         KC_I,     KC_O,         KC_P,     KC_BSLS,
+KC_ESC,   KC_A,         KC_S,     KC_D,     KC_F,      KC_G,      KC_HOME,      KC_DEL,   KC_H,      KC_J,         KC_K,     KC_L,         KC_SCLN,  KC_QUOT,
+KC_LSFT,  KC_Z,         KC_X,     KC_C,     KC_V,      KC_B,      KC_END,       KC_RSFT,  KC_N,      KC_M,         KC_COMM,  KC_DOT,       KC_SLSH,  KC_ENT,
+KC_LCTL,  KC_LGUI,      KC_LALT,  MO(2),    TO(1),     MO(1),     KC_SPC,       KC_SPC,   MO(1),     TO(3),        KC_LEFT,  KC_DOWN,      KC_UP,    KC_RGHT),
 
 	[_FN] = LAYOUT_ortho_5x14(
-	        KC_TRNS, KC_F1,   KC_F2,   KC_F3,   KC_F4,    KC_F5,    KC_F6,         KC_F7,   KC_F8,    KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_TRNS,
-	        KC_TRNS, M_VWQ,   M_VW,    X(EURO), KC_TRNS,  KC_TRNS,  KC_TRNS,       KC_TRNS, KC_TRNS,  XP(UE,UE_U), KC_TRNS, XP(OE,OE_U), KC_PSCR, KC_TRNS,
-	        KC_TRNS, XP(AE,AE_U), X(SZ), KC_TRNS, KC_TRNS,  KC_TRNS,  KC_TRNS,       KC_TRNS, KC_LEFT,  KC_DOWN, KC_UP,   KC_RGHT, KC_TRNS, KC_TRNS,
-	        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS,  KC_TRNS,       KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-	        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, TO(2),    KC_TRNS,  KC_TRNS,       KC_TRNS, KC_TRNS,  TO(0),   KC_TRNS, KC_PGDN, KC_PGUP, KC_TRNS),
+KC_TRNS,  KC_F1,        KC_F2,    KC_F3,    KC_F4,     KC_F5,     KC_F6,        KC_F7,    KC_F8,     KC_F9,        KC_F10,   KC_F11,       KC_F12,   KC_TRNS,
+KC_TRNS,  M_VWQ,        M_VW,     X(EURO),  KC_TRNS,   KC_TRNS,   KC_TRNS,      KC_TRNS,  KC_TRNS,   XP(UE,UE_U),  KC_TRNS,  XP(OE,OE_U),  KC_PSCR,  KC_TRNS,
+KC_TRNS,  XP(AE,AE_U),  X(SZ),    KC_TRNS,  KC_TRNS,   KC_TRNS,   KC_TRNS,      KC_TRNS,  KC_LEFT,   KC_DOWN,      KC_UP,    KC_RGHT,      KC_TRNS,  KC_TRNS,
+KC_TRNS,  KC_TRNS,      KC_TRNS,  KC_TRNS,  KC_TRNS,   KC_TRNS,   KC_TRNS,      KC_TRNS,  KC_TRNS,   KC_TRNS,      KC_TRNS,  KC_TRNS,      KC_TRNS,  KC_TRNS,
+KC_TRNS,  KC_TRNS,      KC_TRNS,  KC_TRNS,  TO(2),     KC_TRNS,   KC_TRNS,      KC_TRNS,  KC_TRNS,   TO(0),        KC_TRNS,  KC_PGDN,      KC_PGUP,  KC_TRNS),
 
-	[_MEDIA] = LAYOUT_ortho_5x14(
-	        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS,  KC_TRNS,       KC_TRNS, KC_TRNS,  KC_TRNS, KC_MUTE, KC_VOLD, KC_VOLU, KC_TRNS,
-	        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS,  KC_TRNS,       KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-	        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS,  KC_TRNS,       KC_TRNS, KC_MS_L,  KC_MS_D, KC_MS_U, KC_MS_R, KC_TRNS, KC_TRNS,
-	        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS,  KC_TRNS,       KC_TRNS, KC_TRNS,  KC_TRNS, KC_BTN1, KC_BTN2, KC_TRNS, KC_TRNS,
-	        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, TO(3),    KC_TRNS,  KC_TRNS,       KC_MPLY, KC_TRNS,  TO(1),   KC_TRNS, KC_MRWD, KC_MFFD, KC_TRNS),
+    [_MEDIA] = LAYOUT_ortho_5x14(
+KC_TRNS,  KC_TRNS,      KC_TRNS,  KC_TRNS,  KC_TRNS,   KC_TRNS,   KC_TRNS,      KC_TRNS,  KC_TRNS,   KC_TRNS,      KC_MUTE,  KC_VOLD,      KC_VOLU,  KC_TRNS,
+KC_TRNS,  KC_TRNS,      KC_TRNS,  KC_TRNS,  KC_TRNS,   KC_TRNS,   KC_TRNS,      KC_TRNS,  KC_TRNS,   KC_TRNS,      KC_TRNS,  KC_TRNS,      KC_TRNS,  KC_TRNS,
+KC_TRNS,  KC_TRNS,      KC_TRNS,  KC_TRNS,  KC_TRNS,   KC_TRNS,   KC_TRNS,      KC_TRNS,  KC_MS_L,   KC_MS_D,      KC_MS_U,  KC_MS_R,      KC_TRNS,  KC_TRNS,
+KC_TRNS,  KC_TRNS,      KC_TRNS,  KC_TRNS,  KC_TRNS,   KC_TRNS,   KC_TRNS,      KC_TRNS,  KC_TRNS,   KC_TRNS,      KC_BTN1,  KC_BTN2,      KC_TRNS,  KC_TRNS,
+KC_TRNS,  KC_TRNS,      KC_TRNS,  KC_TRNS,  TO(3),     KC_TRNS,   KC_TRNS,      KC_MPLY,  KC_TRNS,   TO(1),        KC_TRNS,  KC_MRWD,      KC_MFFD,  KC_TRNS),
 
 	[_SYS] = LAYOUT_ortho_5x14(
-            KC_TRNS, RGB_M_P, RGB_M_B, RGB_M_R, RGB_M_SW, RGB_M_SN, RGB_M_K,       RGB_M_X, RGB_M_G,  KC_TRNS, BL_TOGG, BL_DEC,  BL_INC,  BL_BRTG,
-	        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, RESET,    KC_TRNS,  KC_TRNS,       KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-	        KC_TRNS, KC_TRNS, KC_TRNS, DEBUG,   KC_TRNS,  KC_TRNS,  KC_TRNS,       RGB_TOG, RGB_HUD,  RGB_SAD, RGB_SAI, RGB_HUI, KC_TRNS, KC_TRNS,
-	        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS,  KC_TRNS,       KC_TRNS, RGB_RMOD, RGB_MOD, RGB_SPD, RGB_SPI, KC_TRNS, KC_TRNS,
-	        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, TO(0),    KC_TRNS,  KC_TRNS,       KC_TRNS, KC_TRNS,  TO(2),   KC_TRNS, KC_TRNS, RGB_VAI, RGB_VAD)
+KC_TRNS,  RGB_M_P,      RGB_M_B,  RGB_M_R,  RGB_M_SW,  RGB_M_SN,  RGB_M_K,      RGB_M_X,  RGB_M_G,   KC_TRNS,      BL_TOGG,  BL_DEC,       BL_INC,   BL_BRTG,
+KC_TRNS,  KC_TRNS,      KC_TRNS,  KC_TRNS,  RESET,     KC_TRNS,   KC_TRNS,      KC_TRNS,  KC_TRNS,   KC_TRNS,      KC_TRNS,  KC_TRNS,      KC_TRNS,  KC_TRNS,
+KC_TRNS,  KC_TRNS,      KC_TRNS,  DEBUG,    KC_TRNS,   KC_TRNS,   KC_TRNS,      RGB_TOG,  RGB_HUD,   RGB_SAD,      RGB_SAI,  RGB_HUI,      KC_TRNS,  KC_TRNS,
+KC_TRNS,  KC_TRNS,      KC_TRNS,  KC_TRNS,  KC_TRNS,   KC_TRNS,   KC_TRNS,      KC_TRNS,  RGB_RMOD,  RGB_MOD,      RGB_SPD,  RGB_SPI,      KC_TRNS,  KC_TRNS,
+KC_TRNS,  KC_TRNS,      KC_TRNS,  KC_TRNS,  TO(0),     KC_TRNS,   KC_TRNS,      KC_TRNS,  KC_TRNS,   TO(2),        KC_TRNS,  KC_TRNS,      RGB_VAI,  RGB_VAD)
 };
 
 // RGB
