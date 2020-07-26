4b71e930c12b892dd05e149d3e980a52ea303f50
Thorsten Ott <thorsten.ott@10up.com>
2020-07-26 21:26:23 +0200

    add layer for numpad and neomutt one handed inbox scanning

--- keyboards/keebio/viterbi/keymaps/tott/keymap.c
+++ keyboards/keebio/viterbi/keymaps/tott/keymap.c
@@ -5,6 +5,7 @@ extern rgblight_config_t rgblight_config;
 #define _FN     1
 #define _MEDIA  2
 #define _SYS    3
+#define _ALT    4
 
 // Macro
 enum {
@@ -42,7 +43,7 @@ KC_GRV,   KC_1,         KC_2,     KC_3,     KC_4,      KC_5,      KC_6,         
 KC_TAB,   KC_Q,         KC_W,     KC_E,     KC_R,      KC_T,      KC_LBRC,      KC_RBRC,  KC_Y,      KC_U,         KC_I,     KC_O,         KC_P,     KC_BSLS,
 KC_ESC,   KC_A,         KC_S,     KC_D,     KC_F,      KC_G,      KC_HOME,      KC_DEL,   KC_H,      KC_J,         KC_K,     KC_L,         KC_SCLN,  KC_QUOT,
 KC_LSFT,  KC_Z,         KC_X,     KC_C,     KC_V,      KC_B,      KC_END,       KC_RSFT,  KC_N,      KC_M,         KC_COMM,  KC_DOT,       KC_SLSH,  KC_ENT,
-KC_LCTL,  KC_LGUI,      MO(1),    MO(2),    TO(1),     KC_LALT,   KC_SPC,       KC_SPC,   MO(1),     TO(3),        KC_LEFT,  KC_DOWN,      KC_UP,    KC_RGHT),
+KC_LCTL,  KC_LGUI,      MO(1),    MO(2),    TO(1),     KC_LALT,   KC_SPC,       KC_SPC,   MO(1),     TO(4),        KC_LEFT,  KC_DOWN,      KC_UP,    KC_RGHT),
 
 	[_FN] = LAYOUT_ortho_5x14(
 KC_TRNS,  KC_F1,        KC_F2,    KC_F3,    KC_F4,     KC_F5,     KC_F6,        KC_F7,    KC_F8,     KC_F9,        KC_F10,   KC_F11,       KC_F12,   KC_TRNS,
@@ -63,7 +64,14 @@ KC_TRNS,  RGB_M_P,      RGB_M_B,  RGB_M_R,  RGB_M_SW,  RGB_M_SN,  RGB_M_K,      
 KC_TRNS,  KC_TRNS,      KC_TRNS,  KC_TRNS,  RESET,     KC_TRNS,   KC_TRNS,      KC_TRNS,  KC_TRNS,   KC_TRNS,      KC_TRNS,  KC_TRNS,      KC_TRNS,  KC_TRNS,
 KC_TRNS,  KC_TRNS,      KC_TRNS,  DEBUG,    KC_TRNS,   KC_TRNS,   KC_TRNS,      RGB_TOG,  RGB_HUD,   RGB_SAD,      RGB_SAI,  RGB_HUI,      KC_TRNS,  KC_TRNS,
 KC_TRNS,  KC_TRNS,      KC_TRNS,  KC_TRNS,  KC_TRNS,   KC_TRNS,   KC_TRNS,      KC_TRNS,  RGB_RMOD,  RGB_MOD,      RGB_SPD,  RGB_SPI,      KC_TRNS,  KC_TRNS,
-KC_TRNS,  KC_TRNS,      KC_TRNS,  KC_TRNS,  TO(0),     KC_TRNS,   KC_TRNS,      KC_TRNS,  KC_TRNS,   TO(2),        KC_TRNS,  KC_TRNS,      RGB_VAI,  RGB_VAD)
+KC_TRNS,  KC_TRNS,      KC_TRNS,  KC_TRNS,  TO(4),     KC_TRNS,   KC_TRNS,      KC_TRNS,  KC_TRNS,   TO(2),        KC_TRNS,  KC_TRNS,      RGB_VAI,  RGB_VAD),
+
+	[_ALT] = LAYOUT_ortho_5x14(
+KC_TRNS,  KC_TRNS,      KC_TRNS,  KC_TRNS,  KC_TRNS,   KC_TRNS,   KC_TRNS,      KC_7,     KC_8,      KC_9,         KC_0,     KC_MINS,      KC_EQL,   KC_TRNS,
+KC_TRNS,  KC_Q,         KC_TRNS,  KC_TRNS,  KC_TRNS,   KC_TRNS,   KC_TRNS,      KC_4,     KC_5,      KC_6,         KC_TRNS,  KC_TRNS,      KC_TRNS,  KC_BSLS,
+KC_TRNS,  KC_A,         KC_H,     KC_J,     KC_K,      KC_L,      KC_TRNS,      KC_1,     KC_2,      KC_3,         KC_TRNS,  KC_TRNS,      KC_TRNS,  KC_TRNS,
+KC_TRNS,  KC_TRNS,      KC_TRNS,  KC_TRNS,  KC_TRNS,   KC_TRNS,   KC_TRNS,      KC_TRNS,  KC_TRNS,   KC_TRNS,      KC_COMM,  KC_DOT,       KC_SLSH,  KC_ENT,
+KC_TRNS,  KC_TRNS,      KC_TRNS,  KC_TRNS,  TO(0),     KC_TRNS,   KC_TRNS,      KC_TRNS,  KC_TRNS,   TO(3),        KC_LEFT,  KC_DOWN,      KC_UP,    KC_RGHT)
 };
 
 // RGB
@@ -125,6 +133,10 @@ layer_state_t layer_state_set_user(uint32_t state) {
       case _MEDIA:
         desiredEffect = rgblight_config.mode;
         rgblight_sethsv_noeeprom(HSV_ORANGE);
+        rgblight_mode_noeeprom(1);
+        break;
+      case _ALT:
+        rgblight_sethsv_noeeprom(HSV_YELLOW);
         rgblight_mode_noeeprom(1);
         break;
       case _SYS:
